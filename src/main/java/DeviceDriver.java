75fe3f01ba6ca347271c1395919441af7407ebf4
chlduq04 <chlduq04@gmail.com>
2024-04-30 16:44:04 +0900

    ReadAndPrint and PrintAll

--- src/main/java/DeviceDriver.java
+++ src/main/java/DeviceDriver.java
@@ -1,18 +1,66 @@
+import javax.naming.SizeLimitExceededException;
+import java.util.Vector;
+
 /**
  * This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
  */
+class ReadFailException extends RuntimeException {
+    private static final String DEFAULT_MESSAGE = "Read ERROR!";
+    public ReadFailException() {
+        super(DEFAULT_MESSAGE);
+    }
+}
+
+class WriteFailException extends RuntimeException {
+    private static final String DEFAULT_MESSAGE = "Write ERROR!";
+    public WriteFailException() {
+        super(DEFAULT_MESSAGE);
+    }
+}
+
 public class DeviceDriver {
+    public static final byte EMPTY_BYTE_VALUE = (byte) 0xFF;
+    public static final int READ_DELAY_TIME = 200;
+    private final FlashMemoryDevice hardware;
 
     public DeviceDriver(FlashMemoryDevice hardware) {
-        // TODO: implement this method
+        this.hardware = hardware;
     }
 
     public byte read(long address) {
-        // TODO: implement this method
-        return -1;
+        byte firstTimeResult = hardware.read(address);
+        for (int i = 0; i < 4; i++) {
+            try {
+                Thread.sleep(READ_DELAY_TIME);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (firstTimeResult != hardware.read(address)) {
+                throw new ReadFailException();
+            }
+        }
+        return firstTimeResult;
     }
 
     public void write(long address, byte data) {
-        // TODO: implement this method
+        if (read(address) != EMPTY_BYTE_VALUE) {
+            throw new WriteFailException();
+        }
+        hardware.write(address, data);
+    }
+
+    public Vector<Byte> readAndPrint(long startAddress, long endAddress) {
+        Vector<Byte> result = new Vector<>();
+        for (long nowAddress = startAddress; nowAddress < endAddress; nowAddress++) {
+            result.add(read(nowAddress));
+        }
+        return result;
+    }
+
+    public void writeAll(byte data) {
+        write(0x00, data);
+        write(0x01, data);
+        write(0x02, data);
+        write(0x03, data);
     }
 }
--- src/test/java/DeviceDriverTest.java
+++ src/test/java/DeviceDriverTest.java
@@ -1,14 +1,70 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class DeviceDriverTest {
+    public static final int DEFAULT_TEST_ADDRESS = 0xFF;
+    public static final int DEFAULT_TEST_START_ADDRESS = 0x00;
+    public static final int DEFAULT_TEST_END_ADDRESS = 0x04;
+    public static final byte DEFAULT_TEST_BYTE = (byte) 0xFE;
+    public static final Class READ_FAIL_EXCEPTION_CLASS = ReadFailException.class;
+    public static final Class WRITE_FAIL_EXCEPTION_CLASS = WriteFailException.class;
+
+    @Spy
+    FlashMemoryDevice hardware;
+
+    DeviceDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = new DeviceDriver(hardware);
+    }
 
     @Test
-    public void read_From_Hardware() {
-        // TODO: replace hardware with a Test Double
-        FlashMemoryDevice hardware = null;
-        DeviceDriver driver = new DeviceDriver(hardware);
-        byte data = driver.read(0xFF);
-        assertEquals(0, data);
+    void readAndPrintSuccess() {
+        driver.readAndPrint(DEFAULT_TEST_START_ADDRESS, DEFAULT_TEST_END_ADDRESS);
+
+        verify(hardware, times(20)).read(anyLong());
     }
+
+    @Test
+    void readAndPrintError() {
+        when(hardware.read(DEFAULT_TEST_START_ADDRESS)).thenThrow(new ReadFailException());
+
+        assertThatThrownBy(() -> {
+            driver.readAndPrint(DEFAULT_TEST_START_ADDRESS, DEFAULT_TEST_END_ADDRESS);
+        }).isInstanceOf(READ_FAIL_EXCEPTION_CLASS).hasMessageContaining("ERROR");
+    }
+
+    @Test
+    void writeAllSuccess() {
+        when(hardware.read(anyLong())).thenReturn(DeviceDriver.EMPTY_BYTE_VALUE);
+
+        driver.writeAll(DEFAULT_TEST_BYTE);
+
+        verify(hardware, times(4)).write(anyLong(), anyByte());
+    }
+
+    @Test
+    void writeAllError() {
+        when(hardware.read(anyLong())).thenReturn(DEFAULT_TEST_BYTE);
+
+        assertThatThrownBy(() -> {
+            driver.writeAll(DEFAULT_TEST_BYTE);
+        }).isInstanceOf(WRITE_FAIL_EXCEPTION_CLASS).hasMessageContaining("ERROR");
+    }
+
 }
+
+
